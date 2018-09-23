99cb5cfc1a24b3c3eb37c8227a1d3d32607ec9a2
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2018-09-23 18:26:26 +0200

    added asyn await with promise all

--- routes/index.js
+++ routes/index.js
@@ -9,58 +9,35 @@ let dates = getDate();
 //let day = date[0].date
 //let month = date[0].date
 
-
-
-const todaysDate = function(){
-    dates.forEach(function(date) {
-        console.log(date.date);
-        console.log(date.months)
-    });
-
+const getUrl = async () => {
+    return await Promise.all( dates.map(date => axios.get(`https://api.abalin.net/get/namedays?day=${date.date}=&month=${date.months}`)));
 }
 
-const getUrl = async () => {
-        let day = 1;
-        let month = 1;
-        let urls = axios.get(`https://api.abalin.net/get/namedays?day=${day}=&month=${month}`);
-        console.log(typeof urls); // object
-        console.log(urls); // Promise{<pending>}
-        return urls
-};
-
-const getUrls = async () => {
-    dates.map(date => {
-        let day = date.date;
-        let month = date.months;
-        return axios.get(`https://api.abalin.net/get/namedays?day=${day}=&month=${month}`);
-    });
-};
 const getSaints = async () => {
     try {
-        // destructure returned object instead of calling data.data.name_es
-
-        let { data: { data: { name_es } } } = await getUrl(); //ok
-        // let { data: { data: { name_es } } } = await getUrls(); // not ok
-        // Cannot destructure property `data` of 'undefined' or 'null'.
-        // why is it getting undefined and how do I destructure from a group of urls
-        // should I be using a forEach here as now I have more than one url?
-
-        return name_es;
+        let results = await getUrl()
+        results.map(result => {
+            let { data: { data: { name_es } } } = result;
+            console.log(name_es); // prints all names perfectly
+            return name_es
+        })
     }
     catch (error) {
         console.log(error);
     }
-}
-
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    getSaints() // call then and pass function to then handler to pass result of getSaints() - call it saints as that makes sense
-        .then(function(saints) { res.render('index', {
+    getSaints()
+        .then(function(name_es){
+            console.log(name_es) //undefined
+        })
+        .then(function(name_es) { res.render('index', {
                 days: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
                 months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
             date: dates,
-                saints
+                saints: name_es
             })
         })
 });

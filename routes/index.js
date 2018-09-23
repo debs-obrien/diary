43fbbfdd732fced142d2736626dcae70c395fe52
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2018-09-23 11:30:31 +0200

    trying to get all saints to show

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,56 @@
 var express = require('express');
 var router = express.Router();
-const date = require('../public/javascripts/getDate');
+const getDate = require('../public/javascripts/getDate');
 //const saints = require('../public/javascripts/getSaints');
 const axios = require("axios");
 
+let dates = getDate();
+
+//let day = date[0].date
+//let month = date[0].date
+
+
+
+const todaysDate = function(){
+    dates.forEach(function(date) {
+        console.log(date.date);
+        console.log(date.months)
+    });
+
+}
 
 const getUrl = async () => {
-    return axios.get('https://api.abalin.net/get/namedays?day=11&month=5')
-}
+        let day = 1;
+        let month = 1;
+        let urls = axios.get(`https://api.abalin.net/get/namedays?day=${day}=&month=${month}`)
+        console.log(typeof urls); // object
+        console.log(urls); // Promise{<pending>}
+        return urls
+};
+
+const allUrls = []
+
+const getUrls = async () => {
+    dates.map(date => {
+        let day = date.date;
+        let month = date.months;
+        let urls = axios.get(`https://api.abalin.net/get/namedays?day=${day}=&month=${month}`)
+        //console.log(typeof urls); // object
+        console.log(urls); // Promise{<pending>}
+        //allUrls.push(urls)
+        return urls
+    });
+};
 const getSaints = async () => {
     try {
         // destructure returned object instead of calling data.data.name_es
-        let { data: { data: { name_es } } } = await getUrl();
+
+        let { data: { data: { name_es } } } = await getUrl(); //ok
+        // let { data: { data: { name_es } } } = await getUrls(); // not ok
+        // Cannot destructure property `data` of 'undefined' or 'null'.
+        // why is it getting undefined and how do I destructure from a group of urls
+        // should I be using a forEach here as now I have more than one url?
+
         return name_es;
     }
     catch (error) {
@@ -26,7 +65,7 @@ router.get('/', function(req, res, next) {
         .then(function(saints) { res.render('index', {
                 days: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
                 months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-                curDate: date(),
+            date: dates,
                 saints
             })
         })
